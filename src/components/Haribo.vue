3f8b0d0bf608bf0edf19808555a5b620fb6358d9
이태형 <spdhsrnvl456@gmail.com>
2023-08-19 18:36:02 +0900

    🎨style:이미지설정

---
+++ src/components/Haribo.vue
@@ -0,0 +1,17 @@
+<template>
+      <img src="../assets/haribo.png" />
+</template>
+
+<script>
+export default {
+    name : 'Haribo'
+}
+</script>
+
+<style>
+  img{
+    width: 26vw;
+    height : 26vw;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
+</style>
