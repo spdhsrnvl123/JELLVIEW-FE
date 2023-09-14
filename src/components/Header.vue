13ee0bccb50726a427ede621e5143f3fbb1af002
이태형 <spdhsrnvl456@gmail.com>
2023-09-14 13:33:15 +0900

    ♻︎refactor:불필요한 중복 컴포넌트 제거

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="container2">
-    <Haribo2 />
+  <img src="../assets/haribo.png" />
     <Logo />
   </div>
 </template>
 
 <script>
-import Haribo2 from './Haribo2.vue';
 import Logo from './Logo.vue';
 
 export default {
     components : {
         Logo : Logo,
-        Haribo2 : Haribo2
     }
 }
 </script>
@@ -25,4 +23,9 @@ export default {
       margin-right: 100px;
       padding-top: 12px;
     }
+      img{
+    width: 14vw;
+    height : 14vw;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  }
 </style>
