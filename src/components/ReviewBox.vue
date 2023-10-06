2a63dc897e8fc1db5ab3c3b39b95c8e8fa88226b
이태형 <spdhsrnvl456@gmail.com>
2023-10-06 23:21:55 +0900

    ✨feat:리뷰 공통 컴포넌트 구현

--- src/components/ReviewBox.vue
+++ src/components/ReviewBox.vue
@@ -1,15 +1,18 @@
 <template>
-  <Header />
-  <div class="reviewTop">
-    <button @click="$router.push('/review')" class="reviewText">
+
+<div class="titleBox">
+  <Title :size="9" />
+</div>
+  <div class="reviewTop" >
+    <button :style="{ color : col}" @click="$router.push('/review')" class="reviewText">
       전체 리뷰
     </button>
-    <button @click="useAuth('/myreview')" class="reviewText">나의 리뷰</button>
+    <button :style="{ color : colII}" @click="useAuth('/myreview')" class="reviewText">나의 리뷰</button>
   </div>
   <img class="balloonBundle" src="@/assets/balloon.png" />
-  <img class="balloonBundle2" src="@/assets/balloon.png" />
+  <img class="balloonBundle" src="@/assets/balloon.png" />
+  <!-- content -->
   <div class="mainContent">
-    <!-- <ul class="listBox" v-if="reviewget"> -->
     <ul class="listBox">
       <li v-for="(review, i) in listItems" :key="i">
         <Card :review="review" />
@@ -21,41 +24,58 @@
 <script>
 import Balloon from "@/components/Balloon.vue";
 import Card from "@/components/Card.vue";
-import Header from "@/components/Header.vue";
 import Nav from "@/components/Nav.vue";
+import Title from "@/components/Title.vue"
 
 export default {
-  components: { Header, Nav, Balloon, Card },
+  components: { Nav, Balloon, Card, Title },
   data() {
-    return{
-      reviewget: false
-    }
+    return {
+      reviewget: false,
+      col: "black",
+      colII : "black"
+    };
   },
-  computed : {
-    listItems(){
-      const name = this.$route.name;
-      if(name == 'ReviewPage'){
-        return this.$store.state.review;
+  //네비게이션바 스타일링(칼라)
+  created(){
+      // this.$store.dispatch("FETCH_REVIEW");
+      if(this.$route.name == "ReviewPage"){
+        this.col = "rgb(100, 100, 237)";
+      }else if(this.$route.name == "MyReviewPage"){
+        this.colII = "rgb(100, 100, 237)"
+      }else{
+        this.col = "black";
+        this.colII = "black";
       }
-    }
-  }
-
-  // methods: {
-  //   useAuth(path) {
-  //     if (localStorage.getItem("token")) {
-  //       this.$router.push(path);
-  //     } else {
-  //       alert("로그인해주세요!");
-  //     }
-  //   },
-  // },
+  },
+  computed: {
+    listItems() {
+      const name = this.$route.name;
+      if (name == "ReviewPage") {
+          return this.$store.state.review;
+      } else if(name == "MyReviewPage"){
+        return this.$store.state.myReview;
+      }
+    },
+  },
+  methods: {
+    useAuth(path) {
+      console.log(path);
+      if (localStorage.getItem("token")) {
+        this.$router.push(path);
+      } else {
+        alert("로그인해주세요!");
+      }
+    },
+  },
 };
 </script>
 
-
 <style scoped>
+.titleBox{
+  padding-top: 3%;
+}
 .mainContent {
-  /* height: 52.4vh; */
   width: 59%;
   margin: 0 auto;
   padding: 10px;
@@ -74,7 +94,7 @@ export default {
   text-shadow: 0px 3px 3px rgba(0, 0, 0, 0.15);
   border: 0;
   cursor: pointer;
-  transition: all 0.1s;
+  transition: all 0.2s;
   margin: 10px;
 }
 .reviewText:hover {
@@ -94,7 +114,7 @@ export default {
   right: 0%;
   animation: move 3s linear infinite alternate;
 }
-.balloonBundle2 {
+.balloonBundle:nth-child(1) {
   position: fixed;
   z-index: -1;
   bottom: 0;
@@ -104,4 +124,3 @@ export default {
   animation: move 3s linear infinite alternate;
 }
 </style>
-
